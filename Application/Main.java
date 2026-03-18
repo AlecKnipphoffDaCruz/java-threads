cf891194414f676303e30af0a9437aa64d1cdcf2
AlecCruz <alecknipcruz@gmail.com>
2026-03-18 11:21:41 -0300

    faet: runtime exception while running multiple threads

--- Application/Main.java
+++ Application/Main.java
@@ -4,11 +4,12 @@ import Application.Threads.MultithreadThing;
 
 public class Main {
     public static void main(String[] args) {
-        MultithreadThing myThing = new MultithreadThing();
-        MultithreadThing myThing2 = new MultithreadThing();
 
-        myThing.start();
-        myThing2.start();
+        for(int i = 0; i <= 3; i++){
+            MultithreadThing myThing = new MultithreadThing(i);
+            myThing.start();
+        }
+
 
     }
 }
--- Application/Threads/MultithreadThing.java
+++ Application/Threads/MultithreadThing.java
@@ -1,11 +1,23 @@
 package Application.Threads;
 
+import javax.management.RuntimeErrorException;
+
 public class MultithreadThing extends Thread {
+
+    private int threadNumber;
+
+    public MultithreadThing(int threadNumber) {
+        this.threadNumber = threadNumber;
+    }
 
     @Override
     public void run() {
         for (int i = 0; i <= 5; i++) {
-            System.out.println(i);
+            System.out.println(i + " from thread number: " + threadNumber);
+            
+            if(threadNumber == 3){
+                throw new RuntimeException();
+            }
 
             try {
                 Thread.sleep(1000);
